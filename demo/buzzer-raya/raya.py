4ca706704407d10c08d42b83edc1a943bf13f05f
Oscar Beh <oscarbeh@gmail.com>
2025-03-25 02:57:29 +0800

    Properly handle REST Note

--- demo/buzzer-raya/raya.py
+++ demo/buzzer-raya/raya.py
@@ -6,7 +6,7 @@ Buzzer = 32 #Define the pin of the buzzer
 
 GPIO.setwarnings(False) #Ignore warning
 
-REST = 1
+REST = 0
 NOTE_C4 = 262
 NOTE_D4 = 294
 NOTE_E4 = 330
@@ -24,7 +24,8 @@ NOTE_G5 = 784
 NOTE_A5 = 880
 NOTE_B5 = 988
 
-raya_song = [ NOTE_F4, 8, NOTE_G4, 8, NOTE_A4, 2, NOTE_F4, 8, NOTE_G4, 8, NOTE_F4, 8, NOTE_E4, 2, NOTE_C4, 8, 
+raya_song = [ 
+  NOTE_F4, 8, NOTE_G4, 8, NOTE_A4, 2, NOTE_F4, 8, NOTE_G4, 8, NOTE_F4, 8, NOTE_E4, 2, NOTE_C4, 8, 
   NOTE_E4, 8, NOTE_F4, 8, NOTE_G4, 8, NOTE_F4, 8, NOTE_G4, 8, NOTE_A4, 8, NOTE_AS4, 8, NOTE_D5, 8, 
   NOTE_C5, 8, NOTE_AS4, 8, NOTE_A4, 2, REST, 2, NOTE_F4, 8, NOTE_G4, 8, NOTE_A4, 2, NOTE_F4, 8, 
   NOTE_G4, 8, NOTE_A4, 8, NOTE_C5, 4, NOTE_AS4, 4 , NOTE_G4, 8, NOTE_A4, 8, NOTE_AS4, 8, NOTE_A4, 8, 
@@ -39,14 +40,15 @@ raya_song = [ NOTE_F4, 8, NOTE_G4, 8, NOTE_A4, 2, NOTE_F4, 8, NOTE_G4, 8, NOTE_F
   NOTE_G4, 8, NOTE_F4, 8, NOTE_E4, 2, NOTE_C4, 8, NOTE_E4, 8, NOTE_F4, 8, NOTE_G4, 8, NOTE_F4, 8, NOTE_G4, 8, 
   NOTE_A4, 8, NOTE_AS4, 8, NOTE_D5, 8, NOTE_C5, 8, NOTE_AS4, 8, NOTE_A4, 2, REST, 2, NOTE_F4, 8, NOTE_G4, 8, 
   NOTE_A4, 2, NOTE_F4, 8, NOTE_G4, 8, NOTE_A4, 8, NOTE_C5, 4, NOTE_AS4, 4, NOTE_G4, 8, NOTE_A4, 8, NOTE_AS4, 8, 
-  NOTE_A4, 8, NOTE_F4, 8, NOTE_G4, 8, NOTE_A4, 8, NOTE_G4, 8, NOTE_E4, 8, NOTE_F4, 8, NOTE_G4, 8, NOTE_F4, 2 ]
+  NOTE_A4, 8, NOTE_F4, 8, NOTE_G4, 8, NOTE_A4, 8, NOTE_G4, 8, NOTE_E4, 8, NOTE_F4, 8, NOTE_G4, 8, NOTE_F4, 2 
+]
 
 def setup():
-        GPIO.setmode(GPIO.BOARD)      #Set the GPIO port to BIARD encoding mode    
-        GPIO.setup(Buzzer, GPIO.OUT)  #The pin of the buzzer is set to output mode
-        global Buzz                                            
-        Buzz = GPIO.PWM(Buzzer, 440)   
-        Buzz.start(50) 
+	GPIO.setmode(GPIO.BOARD)      #Set the GPIO port to BIARD encoding mode    
+	GPIO.setup(Buzzer, GPIO.OUT)  #The pin of the buzzer is set to output mode
+	global Buzz                                            
+	Buzz = GPIO.PWM(Buzzer, 440)   
+	Buzz.start(50) 
 
 def run():
 	for i in range(0, len(raya_song), 2):	
@@ -54,13 +56,18 @@ def run():
 		note = raya_song[i+1]
 		dur = (4 / note) * 0.5
 		print(i,fre,note,dur)
-		Buzz.ChangeFrequency(fre)	
-		time.sleep(dur)	
+		if fre>0:
+			Buzz.ChangeFrequency(fre)	
+			time.sleep(dur)	
+		else:
+			Buzz.stop()
+			time.sleep(dur)	
+			Buzz.start(50) 
 
 if __name__ == '__main__':		
-    setup()
-    try:
-        run()
-    except KeyboardInterrupt:  	
-        GPIO.cleanup()
-        print("Ending")
+	setup()
+	try:
+		run()
+	except KeyboardInterrupt:  	
+		GPIO.cleanup()
+		print("Ending")
